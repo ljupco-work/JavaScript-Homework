db01d2040dc09499d495a03eabe4ad1ef93b47ef
ljupco petkovski <ljupcoworkpetkovski@gmail.com>
2022-07-22 16:03:14 +0200

    first commit

---
+++ domasna.js
@@ -0,0 +1,20 @@
+// 1-va zadaca
+
+// 1) integer; 2) string 3) string 4) decimal 5) boolean 6) decimal 7) character 8) string 9) boolean 10) ?
+
+// 2 zadaca
+
+var a = "Zdravo";
+var b = "Ljupco"
+
+console.log(a);
+console.log(b);
+
+// 3 zadaca
+
+var x = 2;
+var y = 3;
+var z = x+y;
+
+console.log(z);
+
---
+++ main.js
@@ -0,0 +1,72 @@
+// 123 -- number
+// "" -- string
+// {} -- object
+// [] -- array
+// function() -- function
+// true/false -- boolean
+
+//napisi kod koj ke:
+
+// sobira dva broja
+function sobiraDvaBroja(a, b) {
+  return a + b;
+}
+
+//pecati broevi od 1 do 10 vertikalno;
+//loop - for, while
+// specificen zbor : for ili  while
+// (nekakov uslov vo vakvi zagradi) vo for imas + increment/decrement
+//for:
+//sekogas se deklarira i inicializira promenliva vo for - zborceto for se odnesuva na taa promenliva
+// pa imame ; - tocka zapirka
+// naredno e nekakov uslov
+// pa imame ; - tocka zapirka
+// na krajot imame inkrement/dekrement
+var nizaSoIminja = ["Ljupco", "Goce", "Presil", "Gordana"];
+//                  '   0   ','  1  ', '  2  ', '  3  '
+
+for (var i = nizaSoIminja.length - 1; i >= 0; i = i - 1) {
+  let imetoVoMomentotNaOvaaIteracija = nizaSoIminja[i];
+  //   if (imetoVoMomentotNaOvaaIteracija[0] === "G") {
+  console.log(imetoVoMomentotNaOvaaIteracija);
+  //   }
+}
+// for pocni od posledniot element  - gledaj do prviot element - i namaluvaj za eden
+//OCE NAS ZA FOR -> OD KADE DO KADE I ZA KOLKU DA SE ZGOLEMUVA/NAMALUVA (i)???
+
+// console.log(a, b, c, d, e, f, g, h, i, j);
+
+//pecati parni broevi od 1 do 10;
+for (let i = 1; i < 11; i = i + 1) {
+  if (i % 2 === 1) {
+    console.log("brojot " + i + " e neparen");
+  }
+}
+
+//pecati ime i prezime so konkatinacija
+
+var firstName = "Ljupco";
+
+var lastName = "Petkovski";
+var fullName = firstName + " " + lastName;
+
+//ispecati tri broja vo opagacki redosled
+var x = 2,
+  y = -11,
+  z = -1;
+
+if (x < y && x < z) {
+  console.log("najmaliot broj e  x = ", x);
+} else if (y < x && y < z) {
+  console.log("najmaliot broj e y = ", y);
+} else {
+  console.log("najmaliot broj e z = ", z);
+}
+
+console.log(sobiraDvaBroja(2, 5));
+
+var a = 1;
+var b = sobiraDvaBroja(8, 9);
+var c = a + b;
+
+// proveri dali zbirot na dva broja e paren;
