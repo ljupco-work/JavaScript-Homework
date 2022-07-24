f543cf69405cc24cf4893257eda0e4dbe1aadf0b
ljupco petkovski <ljupcoworkpetkovski@gmail.com>
2022-07-24 21:30:15 +0200

    sixth task solved

--- domasna.js
+++ domasna.js
@@ -10,7 +10,7 @@ var b = "Ljupco";
 console.log(a);
 console.log(b);
 
-// 3 zadaca
+// 3 zadaca i 4 zadaca
 
 var x = 2;
 var y = 3;
@@ -33,8 +33,21 @@ function doMathOperation(operation, number1, number2) {
   }
 }
 
+// zadaca 5
+
 function convertEuroToDenari(euroAmount) {
   console.log(euroAmount * 61.5);
 }
 
 convertEuroToDenari(100);
+
+// zadaca 6
+
+-1 + 4 * 6
+
+var x = -1;
+var y = 4*6;
+
+var z = x + y;
+
+console.log(z);
