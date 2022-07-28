96dcb355015105b6fb1ff97179e548c347df8b05
ljupco petkovski <ljupcoworkpetkovski@gmail.com>
2022-07-28 14:52:59 +0200

    zadaca 7 so objekti i adresi

--- domasna.js
+++ domasna.js
@@ -43,14 +43,44 @@ convertEuroToDenari(100);
 
 // zadaca 6
 
--1 + 4 * 6
+-1 + 4 * 6;
 
 var x = -1;
-var y = 4*6;
+var y = 4 * 6;
 
 var z = x + y;
 
-console.log(z);
+var two = (35 + 5) % 7;
+var three = 14 + (-4 * 6) / 11;
+var four = 2 + (15 / 6) * 1 - (7 % 2);
+console.log(two);
+console.log(three);
+console.log(four);
+
+// zadaca 7
+// a = value1; b = value2
+function swapVarValues(nekojaAdresa) {
+  var c = nekojaAdresa.imeNaCoek1;
+  nekojaAdresa.imeNaCoek1 = nekojaAdresa.imeNaCoek2;
+  nekojaAdresa.imeNaCoek2 = c;
+  return nekojaAdresa;
+}
+
+
+var solunska11 = {
+  imeNaCoek1: 'Ljupco',
+  imeNaCoek2: 'Goce',
+};
+
+var ime1 = 'Gocee';
+var ime2 = 'Ljupcoo';
+
+console.log("a is: ", solunska11.imeNaCoek1);
+console.log("b is: ", solunska11.imeNaCoek2);
+swapVarValues(solunska11);
+console.log("swapping values...");
+console.log("a is: ", solunska11.imeNaCoek1);
+console.log("b is: ", solunska11.imeNaCoek2);
 
 // zadaca 8
 
@@ -61,6 +91,6 @@ var d = 4;
 
 var e = a + b + c + d;
 
-var f = e/4;
+var f = e / 4;
 
 console.log(f);
